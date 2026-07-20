0ed706b94c1053d9d9ba60baaa762aa4c7890dcd
Breddy9797 <breddy.breddy@mail.ru>
2026-07-20 14:28:01 +0500

    deployga bosladik

--- database.py
+++ database.py
@@ -1,11 +1,14 @@
-import os
-
 import psycopg2
+from load import DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT
 
-
-DATABASE_URL = os.getenv('DATABASE_URL')
 def connection():
-    return psycopg2.connect(DATABASE_URL)
+    return psycopg2.connect(
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS,
+        host=DB_HOST,
+        port=DB_PORT,
+    )
 
 connection().autocommit = True
 
